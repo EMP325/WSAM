17a6e19cdf330ac983ea7956566f839c4d213ca5
EMP325 <58645221+EMP325@users.noreply.github.com>
2023-06-10 22:06:10 +0800

    Delete atorch directory

--- atorch/atorch/__init__.py
+++
@@ -1,5 +0,0 @@
-import os
-
-from .distributed.distributed import init_distributed, local_rank, rank, reset_distributed, world_size
-
-os.environ["PIPPY_PIN_DEVICE"] = "0"
--- atorch/atorch/data/__init__.py
+++
@@ -1,4 +0,0 @@
-from .data_utils import expand_batch_dim, get_sample_batch
-from .preloader import GpuPreLoader, data_to_device
-from .shm_context import ShmData, create_coworker_shm_context
-from .shm_dataloader import ShmDataloader, create_shm_dataloader
